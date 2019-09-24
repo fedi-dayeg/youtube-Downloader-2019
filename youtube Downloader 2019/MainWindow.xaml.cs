b91a0f8cda059293a5212d68dddd84ed8b6e8424
fedi <fedi.dayeg@hotmail.com>
2019-09-25 00:13:18 +0100

    fix some bux

--- youtube Downloader 2019/MainWindow.xaml.cs
+++ youtube Downloader 2019/MainWindow.xaml.cs
@@ -46,11 +46,11 @@ namespace youtube_Downloader_2019
                         {
                             pbDownloadProgress.Visibility = Visibility.Visible;
                             tbInfoProgress.Visibility = Visibility.Visible;
-                            tbInfoProgress.Text = "Patienté...";
+                            tbInfoProgress.Text = "wait...";
                             await downloadmp3();
 
                             //  pbDownloadProgressmp3.Visibility = Visibility.Visible;
-                            tbInfoProgress.Text = "Terminé...";
+                            tbInfoProgress.Text = "Completed...";
                         }
                         else if ((mp3check.IsChecked == false) && (vidcheck.IsChecked == true))
 
@@ -58,12 +58,12 @@ namespace youtube_Downloader_2019
                             pbDownloadProgress.Visibility = Visibility.Visible;
                             tbInfoProgress.Visibility = Visibility.Visible;
 
-                            tbInfoProgress.Text = "Patienté...";
+                            tbInfoProgress.Text = "wait...";
                             await DownloadVideo();
                             pbDownloadProgress.Visibility = Visibility.Visible;
                             tbInfoProgress.Visibility = Visibility.Visible;
 
-                            tbInfoProgress.Text = "Terminé...";
+                            tbInfoProgress.Text = "Completed...";
                         }
                         else if ((mp3check.IsChecked == true) && (vidcheck.IsChecked == true))
                         {
@@ -71,23 +71,23 @@ namespace youtube_Downloader_2019
 
                             tbInfoProgress.Visibility = Visibility.Visible;
 
-                            tbInfoProgress.Text = "Patienté...";
+                            tbInfoProgress.Text = "wait...";
                             await downloadmp3();
                             await DownloadVideo();
-                            tbInfoProgress.Text = "Terminé...";
+                            tbInfoProgress.Text = "Completed...";
                         }
 
-                        if (ExtensionList.SelectedValue != null)
-                        {
+                        //if (ExtensionList.SelectedValue != null)
+                       // {
                             /* DownloadButton.Background = Brushes.LightGreen;
                             DownloadButton.Content = "Video pobrano pomyślnie!";
                              pbDownloadProgress.Visibility = Visibility.Hidden;
                              tbInfoProgress.Visibility = Visibility.Hidden;*/
-                        }
-                        else
-                        {
-                            System.Windows.MessageBox.Show("No file extension selected!");
-                        }
+                     //   }
+                     //   else
+                       // {
+                         //   System.Windows.MessageBox.Show("No file extension selected!");
+                       // }
                     }
                     else
                     {
@@ -104,33 +104,45 @@ namespace youtube_Downloader_2019
 
         }
 
+        /// <summary>
+        /// this method load the information about video passed in the URL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void LoadInfo_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //collapse the visibily of the info
             Info.Visibility = Visibility.Collapsed;
+            //create list of extension
+            //PS: i will add this in the code soon
             IEnumerable<string> listOfExtension = new List<string>();
+            // create list of format
+            //TODO i will add this in the next update, when i will found haw to add the music and video in video because youtubeExplode have no method for this, he can only dowload With Highest Video Quality
             IEnumerable<string> listFormat = new List<string>();
+            //crate the youtube client object
             var client = new YoutubeClient();
             var id = "";
 
             try
             {
+                //parse the id from the URL
                 id = YoutubeClient.ParseVideoId(tbURL.Text);
                 var video = await client.GetVideoAsync(id.ToString());
                 var videoForm = await client.GetVideoMediaStreamInfosAsync(id.ToString());
 
-
+                //show the progress bar
                 pbDownloadProgress.Visibility = Visibility.Visible;
 
                 tbInfoProgress.Visibility = Visibility.Visible;
-                tbInfoProgress.Text = "Patienté!";
+                tbInfoProgress.Text = "wait!";
 
 
                 var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id.ToString());
                 listOfExtension = streamInfoSet.GetAll().Select(s => s.Container.GetFileExtension()).Distinct();
 
-
+                //add the extension to the combobox
                 ExtensionList.ItemsSource = listOfExtension;
-
+                //get the information
                 lblTitle.Text = video.Title;
                 lblDescription.Text = video.Description;
                 lblAuthor.Text = video.Author;
@@ -150,7 +162,13 @@ namespace youtube_Downloader_2019
             }
 
         }
-
+        /// <summary>
+        /// get the ful path and filter the symbole that are in the title, because it will make an exception 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="title"></param>
+        /// <param name="ext"></param>
+        /// <returns></returns>
         private string GetFullPath(string path, string title, string ext)
         {
             string newTitle = "";
@@ -192,7 +210,10 @@ namespace youtube_Downloader_2019
 
             return (path + @"\" + newTitle + ".");
         }
-
+        /// <summary>
+        /// method to download the video
+        /// </summary>
+        /// <returns></returns>
         private async Task DownloadVideo()
         {
             //define the progress bar variable
@@ -216,15 +237,14 @@ namespace youtube_Downloader_2019
             string vidPath = GetFullPath(FBD.SelectedPath, RemoveIllegalPathCharacters(videoTitle.Title + ".mp4"));
             await client.DownloadMediaStreamAsync(streamInfo, vidPath, progress);
         }
+      
 
-        private string RemoveIllegalPathCharacters(string path)
-        {
-            string regexSearch = new string(System.IO.Path.GetInvalidFileNameChars()) +
-                                 new string(System.IO.Path.GetInvalidPathChars());
-            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
-            return r.Replace(path, "");
-        }
 
+        /// <summary>
+        /// method to download the video in MP3 format
+        /// TODO mybe i will add ffmepg to get better quality in the next update
+        /// </summary>
+        /// <returns></returns>
         private async Task downloadmp3()
         {
             var progress = new Progress<double>(p =>
@@ -244,6 +264,19 @@ namespace youtube_Downloader_2019
 
             string audioPath = GetFullPath(FBD.SelectedPath, RemoveIllegalPathCharacters(videoTitle.Title + ".mp3"));
             await client.DownloadMediaStreamAsync(streamInfo2, audioPath, progress);
+        }
+
+        /// <summary>
+        /// method to correct the path 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string RemoveIllegalPathCharacters(string path)
+        {
+            string regexSearch = new string(System.IO.Path.GetInvalidFileNameChars()) +
+                                 new string(System.IO.Path.GetInvalidPathChars());
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
         }
     }
 }
